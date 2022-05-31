5463829897018aa7a4418c920b688f13442fdac8
shivamtiwari0 <tshivam337@gmail.com>
2022-05-31 23:33:42 +0530

    commit on 'main'

--- 16-Asynchronous/script.js
+++ 16-Asynchronous/script.js
@@ -1,6 +1,161 @@
-'use strict';
+"use strict";
 
-const btn = document.querySelector('.btn-country');
-const countriesContainer = document.querySelector('.countries');
+const btn = document.querySelector(".btn-country");
+const countriesContainer = document.querySelector(".countries");
 
 ///////////////////////////////////////
+// https://restcountries.com/v2/
+
+const renderCountry = function (data) {
+  const html = `<article class="country">
+  <img class="country__img" src="${data.flags.svg}" />
+  <div class="country__data">
+    <h3 class="country__name">${data.name.common}</h3>
+    <h4 class="country__region">${data.region}</h4>
+    <p class="country__row"><span>👫</span>${(
+      +data.population / 1000000
+    ).toFixed(1)}M people</p>
+    <p class="country__row"><span>🗣️</span>${
+      Object.values(data.languages)[0]
+    }</p>
+    <p class="country__row"><span>💰</span>${Object.keys(data.currencies)}</p>
+  </div>
+</article>`;
+
+  countriesContainer.insertAdjacentHTML("beforeend", html);
+  countriesContainer.style.opacity = 1;
+};
+
+const renderError = function (msg) {
+  countriesContainer.insertAdjacentText("beforeend", msg);
+  countriesContainer.style.opacity = 1;
+};
+
+/*
+//AJAX hell
+
+const getCountryAndNeighbour = function (country) {
+  //AJAX call country 1
+  const request = new XMLHttpRequest();
+  request.open("GET", `https://restcountries.com/v3.1/name/${country}`);
+  request.send();
+
+  request.addEventListener("load", function () {
+    const [data] = JSON.parse(this.responseText);
+    console.log(data);
+
+    //Render country 1
+    renderCountry(data);
+
+    //Render neighbouring country
+    const neighbour = data.borders?.[0];
+
+    if (!neighbour) return;
+ 
+    //AJAX call neighbour
+    const request2 = new XMLHttpRequest();
+    request2.open("GET", `https://restcountries.com/v3.1/alpha/${neighbour}`);
+    request2.send();
+
+    request2.addEventListener("load", function () {
+      const [data2] = JSON.parse(this.responseText);
+      renderCountry(data2);
+    });
+  });
+};
+
+getCountryAndNeighbour("bharat");
+*/
+
+//AJAX call using promise
+
+// const request = fetch("https://restcountries.com/v3.1/name/bharat");
+// console.log(request);
+
+// const getCountryData = function (country) {
+//   fetch(`https://restcountries.com/v3.1/name/${country}`)
+//     .then(function (response) {
+//       console.log(response);
+//       return response.json(); //json method is available for all the responses coming from fetch.
+//       //json is also an Async function so it returns a promise itself so we've to call then method again.
+//     })
+//     .then(function (data) {
+//       console.log(data);
+//       renderCountry(data[0]);
+//       const neighbour = data[0].borders?.[0];
+
+//       if (!neighbour) return;
+//       //country 2
+//       return fetch(`https://restcountries.com/v3.1/alpha/${neighbour}`);
+//     })
+//     .then((response) => response.json())
+//     .then((data) => renderCountry(data[0]));
+// };
+
+const getJSON = function (url, errorMsg = "Something went wrong") {
+  return fetch(url).then((response) => {
+    console.log(response);
+
+    if (!response.ok) throw new Error(`${errorMsg} (${response.status})`);
+    return response.json();
+  });
+};
+
+const getCountryData = function (country) {
+  getJSON(`https://restcountries.com/v3.1/name/${country}`, "Country not found")
+    .then((data) => {
+      renderCountry(data[0]);
+      const neighbour = data[0].borders?.[0];
+      // const neighbour = "skfjsljl";
+
+      if (!neighbour) throw new Error("No neighbour found!");
+      //country 2
+      return getJSON(
+        `https://restcountries.com/v3.1/alpha/${neighbour}`,
+        "Country not found"
+      );
+    })
+    .then((data) => renderCountry(data[0]))
+    .catch((err) => {
+      console.log(`${err}🥵`);
+      renderError(`Something went wrong🥵🥵🥵 ${err.message}. Try again!`);
+    })
+    .finally(() => (countriesContainer.style.opacity = 1)); //Handling or catching errors
+};
+
+btn.addEventListener("click", function () {
+  getCountryData("bharat");
+});
+
+// getCountryData("dhfdksfk");
+
+const whereAmI = function (lat, lng) {
+  fetch(`https://geocode.xyz/${lat},${lng}?geoit=json`)
+    .then((response) => {
+      if (!response.ok) throw new Error("3 or more requests per second.");
+      console.log(response);
+      return response.json();
+    })
+    .then((data) => {
+      if (data.error) throw new Error(data.error.description);
+      console.log(data);
+      console.log(`You are in ${data.city}, ${data.country}`);
+      return fetch(`https://restcountries.com/v3.1/name/${data.country}`);
+    })
+    .then((response) => {
+      console.log(response);
+      if (!response.ok)
+        throw new Error(`Country not found (${response.status})`);
+
+      return response.json();
+    })
+    .then((data) => {
+      console.log(data[0]);
+      renderCountry(data[0]);
+    })
+    .catch((err) => console.error(err.message));
+};
+
+// whereAmI(52.508, 13.381);
+whereAmI(19.037, 71.873);
+// whereAmI(-33.933, 18.474);
